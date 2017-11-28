d28062e03e81d8e2e01acf1decf7d732543fe156
Anushka Fernando <anushka.rf@gmail.com>
2017-11-28 15:51:23 +1100

    Day2 lab exercise

---
+++ Anushka/wk01/2-tue/index.js
@@ -0,0 +1,17 @@
+// part 01
+//var name = prompt('What is your name ?');
+
+if (name) {
+  console.log('Welcome ' + name);
+}
+
+// part 02
+var input = prompt('What is your prefered meal? (Steak, Fruit salad, Tofurkey, Pork chops)');
+var food_item = input.trim().toLowerCase();
+
+
+if (food_item === 'fruit salad' || food_item === 'tofurkey') {
+  console.log("Vegan Friendly");
+} else {
+  console.log("Vegans Beware!");
+}
